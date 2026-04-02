e0da0a63fdf09b307a5f7bba2100d1984c5f1974
dikshank <65603832+dikshank@users.noreply.github.com>
2026-04-02 13:15:14 +0530

    everything runs now, I have tested for 3000 total steps and multiple (2) seeds

--- dqn-mountaincar/src/env/make_env.py
+++ dqn-mountaincar/src/env/make_env.py
@@ -1,10 +1,12 @@
 import gymnasium as gym
 
-def make_env(env_name, max_episode_steps, seed=None):
-    env = gym.make(env_name)
 
-    if max_episode_steps is not None:
-        env._max_episode_steps = max_episode_steps  # override truncation
+def make_env(env_name, max_episode_steps, seed=None, render_mode=None):
+    env = gym.make(
+        env_name,
+        max_episode_steps=max_episode_steps,
+        render_mode=render_mode  # ✅ add this
+    )
 
     if seed is not None:
         env.reset(seed=seed)
